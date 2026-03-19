f3d4b263a4b1f73ab8ccfec1a63a09423d7337db
MaximumReality <lorith79@gmail.com>
2026-03-19 01:30:29 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,21 +6,20 @@ const ASSETS_TO_CACHE = [
   'https://maximumreality.xyz/echoes_in_the_silicon_wreckage.mp3'
 ];
 
-// Install Event: Save the files to the cache
+// Install: Cache the assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(CACHE_NAME).then(cache => {
       console.log('Maximum Reality: Files Cached');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-// Fetch Event: Serve files from cache if offline
+// Fetch: Serve cached assets if offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Return the cached file, or try to fetch it from the network
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
