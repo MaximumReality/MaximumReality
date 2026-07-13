cdf525c1baf13a0709f9295ddf2e5a178f8fe532
MaximumReality <lorith79@gmail.com>
2026-07-13 15:06:53 -0400

    Add package.json for glen-chat project

---
+++ glen/package.js
@@ -0,0 +1,9 @@
+{
+  "name": "glen-chat",
+  "version": "1.0.0",
+  "private": true,
+  "description": "GLEN - a late Gen X chat companion, powered by Groq's llama-3.3-70b-versatile",
+  "scripts": {
+    "dev": "vercel dev"
+  }
+}
